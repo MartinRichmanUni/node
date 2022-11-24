cab72b51d7ad8b20577dbb4c6a3a02f0a8ed9b07
Martin Richman <martyrichman100@gmail.com>
2022-11-24 14:37:02 +0000

    Node Server added with connection to database

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 19007
+
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Succesfully connected to PlanetScale!");
+  });
+
+app.get('/users', (req, res) => {
+    connection.query("SELECT * FROM users",(err, result) => {
+      if (err) {
+        console.log(err)
+      } else {
+        res.send(result)
+      }
+    });
+  })
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
